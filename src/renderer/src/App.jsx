137d8f7d53ea344e76515ac9b7a6a402a32c1ee2
Quang Nguyen <ndq.6895a@gmail.com>
2023-04-28 19:17:56 +0700

    Create Project

---
+++ src/renderer/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container flex h-screen w-screen justify-center items-center">
+      <p className="font-black text-4xl">Hello World</p>
+    </div>
+  )
+}
+
+export default App
